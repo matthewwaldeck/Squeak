d9ab3e723a18b70c7a455772f75544751b6ed3c9
Matt <mattwaldeck@protonmail.com>
2024-06-05 16:04:38 -0400

    v1.1.0
    Created "Diagnostics" section, it is fully functional. Started building other sections to be enabled later, once library scripts are in place.

--- squeak.bat
+++ squeak.bat
@@ -1,28 +1,26 @@
 @echo off
+:main
+cls
 :::       ______                         _    
 :::      / / ___|  __ _ _   _  ___  __ _| | __
 :::     / /\___ \ / _` | | | |/ _ \/ _` | |/ /
 :::  _ / /  ___) | (_| | |_| |  __/ (_| |   < 
 ::: (_)_/  |____/ \__, |\__,_|\___|\__,_|_|\_\
 :::                  |_|                        
-:::Version: 1.0.0
+:::Version: 1.1.0
 :::Developed by: Matt Waldeck
-:::Last update: 2024.06.04
+:::Last update: 2024.06.05
 
 :: Logo display.
 :: Artwork: https://patorjk.com/software/taag/#p=display&f=Standard&t=.%2FSqueak
 for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo %%A
-echo.
-pause
 
-:: Main Menu
-:menu
-cls
+echo.
 echo Welcome to Squeak!
 echo A friendly little utility for setup and maintenance of a Windows system.
 echo.
-echo 1:AV Removal  2:Malware Scan  3:Remove Windows Bloat
-echo 4:System Maintenance  5:Install Software  0:Close
+echo 1:Diagnostics  2:AV Removal  3:Malware Scan  4:Install Software
+echo 5:System Maintenance  6:Remove Windows Bloat  0:Close
 echo.
 set "option="
 set /p "option=What would you like to do today? "
@@ -31,44 +29,87 @@ set /p "option=What would you like to do today? "
 if %option%==0 (
     echo Closing...
     timeout 1 > NUL
+    cls
     exit
-) if %option%==1 (
-    :: Antivirus Removal
+)
+
+:: Diagnostics
+if %option%==1 (
+    mkdir "C:\Temp\Squeak\Diagnostic" > NUL 2>&1
+    echo.
+
+    :: Basic hardware rundown.
+    wmic cpu get name, deviceid, numberofcores, maxclockspeed, status > C:\Temp\Squeak\Diagnostic\Hardware.txt
+    wmic computersystem get totalphysicalmemory >> C:\Temp\Squeak\Diagnostic\Hardware.txt
+    wmic bios get serialnumber >> C:\Temp\Squeak\Diagnostic\Hardware.txt
+
+    :: System info.
+    systeminfo > C:\Temp\Squeak\Diagnostic\SystemInfo.txt
+    wmic path softwareLicensingService get OA3xOriginalProductKey > C:\Temp\Squeak\Diagnostic\Activation.txt
+
+    :: Attached disks.
+    wmic diskdrive get name,model,size > C:\Temp\Squeak\Diagnostic\Disks.txt
+
+    :: Installed software.
+    echo Gathering software information. This will take some time...
+    wmic product get name,version > C:\Temp\Squeak\Diagnostic\Software.txt
+    echo.
+    echo Diagnostics complete. Please see C:\Temp\Squeak\Diagnostic.
+    echo.
+    pause
+    goto main
+)
+
+:: Antivirus Removal
+if %option%==2 (
     echo Starting antivirus removal script...
     timeout 2 > NUL
     start .\av_removal\av_removal.bat
     timeout 2 > NUL
-    goto menu
-) if %option%==2 (
-    :: Malware Scan
+    goto main
+)
+
+:: Malware Scan
+if %option%==3 (
     echo Virus Removal tool is in development!
+    :: mkdir "C:\Temp\Squeak\Malware Removal" > NUL 2>&1
     :: https://support.kaspersky.com/kvrt2015/howto/8537
-    ::echo Downloading Kaspersky Virus Removal Tool...
+    :: echo Downloading Kaspersky Virus Removal Tool...
     :: Need to track down download link.
-    ::echo Starting malware scan...
-    ::call ".\malware_scans\KVRT.exe" -accepteula -silent -processlevel 2
-    ::echo Scan complete! Press enter to return to the main menu.
-    pause > NUL 
-    goto menu
-) if %option%==3 (
-    :: Remove Bloatware
-    echo Bloatware removal script is in development!
+    :: curl https://downfromhere.kaspersky.com/
+    :: echo Starting malware scan...
+    :: call "C:\Temp\Squeak\Malware Removal\KVRT.exe" -accepteula -silent -processlevel 2
+    :: echo Scan complete! Press enter to return to the main main.
     pause
-    goto menu
-) if %option%=4 (
-    :: System Maintenance
-    echo System maintenance script is in development!
-    pause
-    goto menu
-) if %option%=5 (
-    :: Install Software
+    goto main
+)
+
+:: Install Software
+if %option%==4 (
     echo Install scripts are in development!
     :: powershell.exe -executionpolicy unrestricted ./setup.ps1
     pause
-    goto menu
-) else (
-    :: Otherwise...
+    goto main
+)
+
+:: System Maintenance
+if %option%==5 (
+    echo System maintenance script is in development!
+    :: powershell start-process powershell.exe -File .\library\maintenance.ps1 -verb runas
+    pause
+    goto main
+)
+
+:: Remove Bloatware
+if %option%==6 (
+    echo Bloatware removal script is in development!
+    pause
+    goto main
+)
+
+:: Otherwise...
+if %option% GTR 6 (
     echo Please enter a valid selection.
     timeout 3 > NUL
-    goto menu
+    goto main
 )
