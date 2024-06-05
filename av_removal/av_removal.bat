5168d19f67591d14dca7774eeedfdaa290668a8c
Matt <mattwaldeck@protonmail.com>
2024-06-05 16:21:39 -0400

    Complete rewrite
    Rewrote structure from scratch. Now runs without errors. Now to test. Once testing is complete, component will be removed from BETA.

--- av_removal/av_removal.bat
+++ av_removal/av_removal.bat
@@ -7,7 +7,7 @@ cls
 ::: (_)_/  |____/ \__, |\__,_|\___|\__,_|_|\_\
 :::                  |_|                        
 :::Antivirus Removal Tool
-:::Version: 1.0.2 BETA
+:::Version: 1.1.0 BETA
 :::Developed by: Matt Waldeck
 :::Last update: 2024.06.05
 
@@ -16,73 +16,82 @@ cls
 for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo %%A
 echo.
 
-pause
-
-set "remove_av="
-
 :: AVG
 if exist "%PROGRAMFILES%\AVG" (
-    set /p "remove_av=AVG has been detected. Would you like to remove it? "
-    if /I "%remove_av%"=="y" (
-        echo Removing AVG...
-        start ".\av_removal\avgclear.exe"
-    )
+    echo AVG Detected!
+    start ".\av_removal\avgclear.exe"
+    goto eof
+)
+if exist "%PROGRAMFILES(X86)%\AVG" (
+    echo AVG Detected!
+    start ".\av_removal\avgclear.exe"
     goto eof
 )
 
 :: Bitdefender
 if exist "%PROGRAMFILES%\Bitdefender" (
-    set /p "remove_av=Bitdefender has been detected. Would you like to remove it? "
-    if /I "%remove_av%"=="y" (
-        echo Removing Bitdefender...
-        start ".\av_removal\Bitdefender.exe"
-    )
+    echo Bitdefender Detected!
+    start ".\av_removal\Bitdefender.exe"
+    goto eof
+)
+if exist "%PROGRAMFILES(X86)%\Bitdefender" (
+    echo Bitdefender Detected!
+    start ".\av_removal\Bitdefender.exe"
     goto eof
 )
 
 :: Kaspersky
-if exist /I "%PROGRAMFILES%\Kaspersky Lab"(
-    set /p "remove_av=Kaspersky has been detected. Would you like to remove it? "
-    if /I "%remove_av%"=="y" (
-        echo Removing Kaspersky...
-        start ".\av_removal\kavremvr.exe"
-        :: setup_kes.exe /s /x
-    )
+if exist "%PROGRAMFILES%\Kaspersky Lab" (
+    echo Kaspersky Detected!
+    :: start ".\av_removal\kavremvr.exe"
+    setup_kes.exe /s /x
+    goto eof
+)
+if exist "%PROGRAMFILES(X86)%\Kaspersky Lab" (
+    echo Kaspersky Detected!
+    :: start ".\av_removal\kavremvr.exe"
+    setup_kes.exe /s /x
     goto eof
 )
 
 :: McAfee
+if exist "%PROGRAMFILES%\McAfee" (
+    echo McAfee Detected!
+    start ".\av_removal\MCPR.exe"
+    goto eof
+)
 if exist "%PROGRAMFILES(X86)%\McAfee" (
-    set /p "remove_av=McAfee has been detected. Would you like to remove it? "
-    if /I "%remove_av%"=="y" (
-        echo Removing McAfee...
-        start ".\av_removal\MCPR.exe"
-    )
+    echo McAfee Detected!
+    start ".\av_removal\MCPR.exe"
     goto eof
 )
 
 :: Norton (all versions)
-if exist "%PROGRAMFILES(X86)%\Norton*" (
-    set /p "remove_av=Norton has been detected. Would you like to remove it? "
-    if /I "%remove_av%"=="y" (
-        echo Removing Norton...
-        start ".\av_removal\NRnR.exe"
-    )
+if exist "%PROGRAMFILES%\McAfee" (
+    echo Norton Detected!
+    start ".\av_removal\NRnR.exe"
+    goto eof
+)
+if exist "%PROGRAMFILES(X86)%\McAfee" (
+    echo Norton Detected!
+    start ".\av_removal\NRnR.exe"
     goto eof
 )
 
 :: Spybot
-if exist "%PROGRAMFILES%\Spybot - Search & Destroy 2" (
-    set /p "remove_av=Spybot - S&D2 has been detected. Would you like to remove it? "
-    if /I "%remove_av%"=="y" (
-        echo Removing Spybot - Search & Destroy 2...
-        start "%PROGRAMFILES%\Spybot - Search & Destroy 2\unins000.exe"
-    )
+if exist "%PROGRAMFILES%\McAfee" (
+    echo Spybot S&D2 Detected!
+    start "%PROGRAMFILES%\Spybot - Search & Destroy 2\unins000.exe"
+    goto eof
+)
+if exist "%PROGRAMFILES(X86)%\McAfee" (
+    echo Spybot S&D2 Detected!
+    start "%PROGRAMFILES%\Spybot - Search & Destroy 2\unins000.exe"
     goto eof
 )
 
 echo No supported antivirus found. Returning to main menu...
-timeout 3
+timeout 3 > NUL
 
 :eof
 exit
